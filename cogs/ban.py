5121a3404d665475bff50649e483b28d818739c6
SpritesDevelopments <123204447+SpritesDevelopments@users.noreply.github.com>
2023-06-25 10:27:19 +0000

    Update V0.2.4 Bug fix
    Fix issue with ban command giving error
    Next update will include
    Categories for commands and help command system.
    Add games category and add up to 5 games

--- cogs/ban.py
+++ cogs/ban.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from discord_slash import cog_ext, SlashContext
-from config import LOG_CHANNEL_ID
+from config import get_log_channel_id
 
 class Ban(commands.Cog):
     def __init__(self, bot):
@@ -10,12 +10,16 @@ class Ban(commands.Cog):
     @cog_ext.cog_slash(name="ban", description="Ban a user")
     async def ban(self, ctx: SlashContext, member: discord.Member, reason=None):
         await member.ban(reason=reason)
-        log_channel = self.bot.get_channel(LOG_CHANNEL_ID)
-        if log_channel:
-            await log_channel.send(f'{member} was banned. Reason: {reason}')
-            await ctx.send(f'{member} was banned. Reason: {reason}')
+        log_channel_id = get_log_channel_id()
+        if log_channel_id:
+            log_channel = self.bot.get_channel(log_channel_id)
+            if log_channel:
+                await log_channel.send(f'{member} was banned. Reason: {reason}')
+                await ctx.send(f'{member} was banned. Reason: {reason}')
+            else:
+                await ctx.send('Log channel not found.')
         else:
-            await ctx.send('Log channel not found.')
+            await ctx.send('Log channel ID is not set.')
 
 def setup(bot):
     bot.add_cog(Ban(bot))
