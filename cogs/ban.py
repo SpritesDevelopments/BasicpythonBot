00708aefb7faf55b374a7487ce249d73ab7dd77a
SpritesDevelopments <123204447+SpritesDevelopments@users.noreply.github.com>
2023-06-25 11:08:01 +0000

    Update V0.2.5
    Bug fix on ban.py

--- cogs/ban.py
+++ cogs/ban.py
@@ -1,25 +1,47 @@
 import discord
 from discord.ext import commands
 from discord_slash import cog_ext, SlashContext
+from discord_slash.utils.manage_commands import create_option
 from config import get_log_channel_id
 
 class Ban(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @cog_ext.cog_slash(name="ban", description="Ban a user")
-    async def ban(self, ctx: SlashContext, member: discord.Member, reason=None):
-        await member.ban(reason=reason)
-        log_channel_id = get_log_channel_id()
-        if log_channel_id:
-            log_channel = self.bot.get_channel(log_channel_id)
-            if log_channel:
-                await log_channel.send(f'{member} was banned. Reason: {reason}')
-                await ctx.send(f'{member} was banned. Reason: {reason}')
+    @cog_ext.cog_slash(
+        name="ban",
+        description="Ban a user",
+        options=[
+            create_option(
+                name="user",
+                description="The user to ban",
+                option_type=6,
+                required=True
+            ),
+            create_option(
+                name="reason",
+                description="Reason for the ban",
+                option_type=3,
+                required=False
+            )
+        ]
+    )
+    async def ban(self, ctx: SlashContext, user: discord.User, reason=None):
+        member = ctx.guild.get_member(user.id)
+        if member is not None:
+            await member.ban(reason=reason)
+            log_channel_id = get_log_channel_id()
+            if log_channel_id:
+                log_channel = self.bot.get_channel(log_channel_id)
+                if log_channel:
+                    await log_channel.send(f'{user} was banned. Reason: {reason}')
+                    await ctx.send(f'{user} was banned. Reason: {reason}')
+                else:
+                    await ctx.send('Log channel not found.')
             else:
-                await ctx.send('Log channel not found.')
+                await ctx.send('Log channel ID is not set.')
         else:
-            await ctx.send('Log channel ID is not set.')
+            await ctx.send('User not found in the server.')
 
 def setup(bot):
     bot.add_cog(Ban(bot))
